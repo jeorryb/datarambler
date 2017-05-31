e5681c4f3f43ea67ddd2fd3c9f5e1dbe294972d9
jeorryb <jeorry@gmail.com>
2017-05-31 16:23:20 -0400

    add publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -2,33 +2,58 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-# This file is only used if you use `make publish` or
-# explicitly specify it as your config file.
+AUTHOR = 'Jeorry Balasabas'
+SITENAME = 'DataRambler'
+SITEURL = 'https://jeorryb.github.io/datarambler/'
+THEME = 'themes/pelican-bootstrap3'
+TYPOGRIFY = True
+MARKDOWN = {
+    'extension_configs': {
+        'markdown.extensions.codehilite': {'css_class': 'highlight'},
+        'markdown.extensions.extra': {},
+        'markdown.extensions.meta': {},
+    },
+    'output_format': 'html5',
+}
+PLUGINS = ['i18n_subsites', 'ipynb.markup']
+PLUGIN_PATHS = ['plugins']
+JINJA_ENVIRONMENT = {
+    'extensions': ['jinja2.ext.i18n'],
+}
+PATH = 'content'
+DISPLAY_CATEGORIES_ON_MENU = False
+BOOTSTRAP_THEME = 'simplex'
+BOOTSTRAP_FLUID = True
+BANNER = 'images/graph_banner.jpg'
+TWITTER_USERNAME = '@jeorryb'
+TWITTER_WIDGET_ID = 'jeorryb'
+MARKUP = ('md', 'ipynb')
 
-import os
-import sys
-sys.path.append(os.curdir)
-from pelicanconf import *
 
-SITEURL = 'http://www.datarambler.com'
-RELATIVE_URLS = False
+TIMEZONE = 'America/New_York'
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+DEFAULT_LANG = 'en'
 
-DELETE_OUTPUT_DIRECTORY = True
-PLUGIN_PATHS = ['./plugins']
-PLUGINS = ['extract_toc', 'render_math', 'disqus_static', 'better_figures_and_images']
-MD_EXTENSIONS = ['codehilite', 'extra', 'smarty', 'toc']
-PATH = '../raw'
-OUTPUT_PATH = '../www.datarambler.com/'
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+
+
+# Social widget
+SOCIAL = (('github', 'https://github.com/jeorryb'),
+          ('twitter', 'https://twitter.com/jeorryb'),
+          ('linkedin', 'https://www.linkedin.com/in/jeorry'))
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
 STATIC_PATHS = ['extra/robots.txt', 'images', 'extra/favicon.ico']
 EXTRA_PATH_METADATA = {
     'extra/robots.txt': {'path': 'robots.txt'},
     'extra/favicon.ico': {'path': 'favicon.ico'}
-
-
-# Following items are often useful when publishing
-
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+}
