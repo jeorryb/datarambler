1968ccdc018182f3ad9d34ffaf7c743cebc92357
jeorryb <jeorry@gmail.com>
2018-02-13 13:24:59 -0500

    fix rss feed settings

--- publishconf.py
+++ publishconf.py
@@ -40,8 +40,8 @@ DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
 FEED_DOMAIN = SITEURL
-FEED_RSS = 'feeds/rss.xml'
-FEED_ALL_ATOM = None
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
